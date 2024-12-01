d91e7097746f5de5a9d33efcaac4f3fee524eaa6
Lander <55660647+FelixLeander@users.noreply.github.com>
2024-12-01 09:01:06 +0100

    Add project files.

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,34 @@
+﻿using AdventOfCode;
+using System.Drawing;
+using System.Text;
+
+var unsortedNums =
+    Encoding.UTF8.GetString(Resource.InputD1)
+    .Split('\n')
+    .SkipLast(1)
+    .Select(s =>
+    {
+        var split = s.Split(' ', StringSplitOptions.TrimEntries);
+        return new Point(int.Parse(split.First()), int.Parse(split.Last()));
+    })
+    .ToList();
+
+#if D1P1
+var xSort = unsortedNums.OrderBy(o => o.X).ToList();
+var ySort = unsortedNums.OrderBy(o => o.Y).ToList();
+int solution = 0;
+for (int i = 0; i < ySort.Count; i++)
+{
+    var diff = (xSort[i].X - ySort[i].Y);
+    if (diff <= 0)
+        diff *= -1;
+
+    solution += diff;
+}
+
+Console.WriteLine(solution);
+#elif D1P2
+
+unsortedNums.
+#endif
+
